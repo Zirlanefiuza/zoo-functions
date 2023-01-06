fdb99d7297c7dc78a2ffdddff5397f8657e980d1
Zirlanefiuza <zirlanebatistafiuza2@gmail.com>
2023-01-05 23:27:39 -0300

    requisito 5 concluido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,9 +1,19 @@
-const data = require('../data/zoo_data');
-
+const { species } = require('../data/zoo_data');
+// array.hof((elemento, index) => )
+// objeto[chave] = valor
 const countAnimals = (animal) => {
-  if (animal === 'species' || animal === 'sex') {
-    return data.species.filter((especie) => especie.animal === animal).length; 
+  if (!animal) {
+    const listAnimal = species.reduce((acc, crr) => {
+      acc[crr.name] = crr.residents.length;
+      return acc;
+    }, {});
+    return listAnimal;
   }
+  const nomeSpecie = species.find((specie) => specie.name === animal.species);
+  if (animal.sex) {
+    const gender = nomeSpecie.residents.filter((residente) => residente.sex === animal.sex);
+    return gender.length;
+  }
+  return nomeSpecie.residents.length;
 };
 module.exports = countAnimals;
-  
