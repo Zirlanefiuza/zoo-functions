486b87adb7128ebebe3ec3ce26c8436a45cfbc93
Zirlanefiuza <zirlanebatistafiuza2@gmail.com>
2023-01-09 18:29:44 -0300

    requisito 10 concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
+
+const getEmployee = (id) => employees.find((employee) => employee.id === id).responsibleFor[0];
+
+const getAnimalById = (idAnimal) => species.find(({ id }) => id === idAnimal).residents;
+
+const getOlderAnimal = (residents) => residents.reduce((acc, curr) => {
+  if (acc.age > curr.age) return acc;
+  return curr;
+});
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const respons = getEmployee(id);
+  const residents = getAnimalById(respons);
+  return Object.values(getOlderAnimal(residents));
 };
 
 module.exports = getOldestFromFirstSpecies;
